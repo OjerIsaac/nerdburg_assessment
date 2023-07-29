87d4e744dedc2cbe5bb003a367d5aa745fb06283
Ojer Isaac <isaacojerumu@gmail.com>
2023-07-29 01:25:03 +0100

    feat: user migration

---
+++ src/migrations/1690585150288-users.ts
@@ -0,0 +1,26 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class Users1690585150288 implements MigrationInterface {
+  name = 'Users1690585150288';
+
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `CREATE TABLE users (
+                            id CHAR(36) NOT NULL,
+                            firstName VARCHAR(100) NULL UNIQUE,
+                            lastName VARCHAR(100) NULL UNIQUE,
+                            email VARCHAR(100) NOT NULL UNIQUE,
+                            role VARCHAR(50) NOT NULL,
+                            password TEXT NULL,
+                            createdAt TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+                            updatedAt TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+                            deletedAt TIMESTAMP NULL DEFAULT NULL,
+                            PRIMARY KEY (id)
+                          )`
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`DROP TABLE "users" CASCADE`);
+  }
+}
